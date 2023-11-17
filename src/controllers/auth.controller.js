ebc68a545ef0bf6b054980c796fa4baae8da2e8b
Benito Enriquez Mora <enriquez.ico@gmail.com>
2023-11-16 22:54:40 -0600

    feature(token-create): add function for create a acces token.
    add dependencies: jsonwebtoken, bcryptjs.
    files modified: package.json, package-lock.json,
    src/schemas/user.model.js
    config token secret: src/config.js
    use function for create token: src/controllers/auth.controller.js
    function created for crear a access token: src/lib/jwt.js
    
    issue(XXXX).

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,32 @@
-import User from '../schemas/user.model.js';
+import bcrypt from 'bcryptjs';
 
-export const register = (req, res) => {
+import User from '../schemas/user.model.js';
+import { createAccessToken } from '../libs/jwt.js';
+
+export const register = async (req, res) => {
+    const { email, password, username } = req.body;
+    //  generate hash for password
+    const passwordHash = await bcrypt.hash(password, 10);
     //  save data into db mongo use user model
     //  TO DO validate data request body
-    const userToRegister = new User(req.body);
+    const userToRegister = new User({
+        email,
+        password: passwordHash,
+        username
+    });
+
     try {
-        userToRegister.save();
-        res.status(201).send({ status: 201, message: 'OK', data: userToRegister});
+        const userSaved = await userToRegister.save();
+        const token = await createAccessToken({id: userSaved._id});
+        const userResponse = {
+            id: userSaved._id,
+            email: userSaved.email,
+            username: userSaved.username,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt
+        };
+        res.cookie('token', token);
+        res.status(201).send({ status: 201, message: 'OK', data: userResponse});
     } catch (error) {
         res.status(500).send({ status: 500, message: 'FAILED', data: { error: error } })
     }
