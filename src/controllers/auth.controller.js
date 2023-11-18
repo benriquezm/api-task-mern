f2780a216ecdbe10caf0e55441bda7687da79dea
Benito Enriquez Mora <enriquez.ico@gmail.com>
2023-11-17 18:43:52 -0600

    feature(create-login): add route for login and logout.
    files modified: src/controllers/auth.controller.js - add method for process login and logout
    files modified: src/routes/auth.routes.js - add routes for login and logout
    
    issue(XXXX).

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,4 +32,31 @@ export const register = async (req, res) => {
     }
 };
 
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userFound = await User.findOne({ email });
+        if(!userFound) return res.status(404).send({ status: 404, message: 'User not found.', data: {} });
+        //  compare password into db save
+        const isValidPassword = await bcrypt.compare(password, userFound.password);
+        if(!isValidPassword) return res.status(401).send({ status: 401, message: 'No credentials provided or credentials are invalid.', data: {} });
+        const token = await createAccessToken({id: userFound._id});
+        const userResponse = {
+            id: userFound._id,
+            email: userFound.email,
+            username: userFound.username,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        };
+        res.cookie('token', token);
+        res.status(201).send({ status: 201, message: 'OK', data: userResponse});
+    } catch (error) {
+        res.status(500).send({ status: 500, message: 'FAILED', data: { error: error } })
+    }
+};
+
+export const logout = (req, res) => {
+    res.cookie('token', '', { expires: new Date(0) });
+    res.status(200).send({ status: 200, message: 'session ended successfully.', data: {}});
+};
